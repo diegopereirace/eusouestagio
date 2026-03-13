5d3a752a55d05f234e77001bb2abd68123af61fb
Diego Pereira <151082544+diegopereirace@users.noreply.github.com>
2026-03-13 16:13:00 -0300

    bootstrap

--- themes/custom/default/assets/js/global.js
+++ themes/custom/default/assets/js/global.js
@@ -1,15 +1,10 @@
-/**
- * @file
- * Global utilities.
- *
- */
 (function (Drupal) {
 
   'use strict';
 
   Drupal.behaviors.bootstrap_barrio_subtheme = {
     attach: function (context, settings) {
-
+      console.log('teste');
     }
   };
 

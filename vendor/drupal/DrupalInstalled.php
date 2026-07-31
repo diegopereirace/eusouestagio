dc2201357e4268cf6bde76ebe796d6a7e7e064a3
Diego Pereira <151082544+diegopereirace@users.noreply.github.com>
2026-07-31 11:47:48 -0300

    update ctools

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '6754f9d75076eba4';
+  public const string VERSIONS_HASH = 'bc94505b7f282db7';
 
 }
